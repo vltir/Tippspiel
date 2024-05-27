71ed17577293d5632ad585dd602485926c113d53
Etienne Bader <baderetienne@gmail.com>
2024-05-27 15:45:51 +0200

    initial commit

---
+++ src/main/java/de/dhbwka/tippspiel/TippspielApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwka.tippspiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TippspielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TippspielApplication.class, args);
+	}
+
+}
