d7bd8a1cfe37a4977fa898d9d7703967b26cb890
vltir <87820976+vltir@users.noreply.github.com>
2024-05-28 10:22:57 +0200

    Added constructors to User.java

--- src/main/java/de/dhbwka/tippspiel/entities/User.java
+++ src/main/java/de/dhbwka/tippspiel/entities/User.java
@@ -12,6 +12,13 @@ public class User {
     private String username;
     private String password;
 
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     // Getters and setters
     public Long getId() {
         return id;
