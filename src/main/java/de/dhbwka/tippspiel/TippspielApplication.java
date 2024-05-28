4c6c2f91bd8d2daddbac2e40719640d3af7c6287
vltir <87820976+vltir@users.noreply.github.com>
2024-05-28 10:23:59 +0200

    Added Annotations to TippspielApplication.java to reference entity and repository package

--- src/main/java/de/dhbwka/tippspiel/TippspielApplication.java
+++ src/main/java/de/dhbwka/tippspiel/TippspielApplication.java
@@ -2,8 +2,12 @@ package de.dhbwka.tippspiel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "de.dhbwka.tippspiel.entities")
+@EnableJpaRepositories(basePackages = "de.dhbwka.tippspiel.repositories")
 public class TippspielApplication {
 
 	public static void main(String[] args) {
