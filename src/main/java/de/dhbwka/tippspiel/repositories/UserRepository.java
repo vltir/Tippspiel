afbdcca61a0f3bf7132d3e6a50470b48472ef50d
vltir <87820976+vltir@users.noreply.github.com>
2024-05-28 12:07:30 +0200

    added repository Annotation to UserRepository.java

--- src/main/java/de/dhbwka/tippspiel/repositories/UserRepository.java
+++ src/main/java/de/dhbwka/tippspiel/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package de.dhbwka.tippspiel.repositories;
 import de.dhbwka.tippspiel.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
