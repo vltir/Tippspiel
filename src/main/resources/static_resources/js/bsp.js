64dbb7dac439d06346977732846273c2d051acc3
dav1d737 <84008059+dav1d737@users.noreply.github.com>
2024-06-11 15:05:01 +0200

    js fix for html :)

--- src/main/resources/static_resources/js/bsp.js
+++ src/main/resources/static_resources/js/bsp.js
@@ -4,5 +4,37 @@ let login = document.querySelector('#login');
 let sinup = document.querySelector('#signup');
 let form = document.querySelector('#form');
 
+// Event: Wenn jemand auf den Container "login | signup" klickt wechselt die Seite von login auf signup und zurück
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraph = document.getElementById("switch");
+    paragraph.addEventListener("click", function() {
+
+       if(count == 0){
+           count = 1;
 
 
+           button.innerHTML = "sign up";
+          button.name = 'signup';
+
+
+
+
+       } else if(count == 1){
+        count = 0;
+
+
+
+
+        button.innerHTML = "login";
+        form.action = 'login';
+
+
+
+
+
+
+    }
+
+
+    });
+});
