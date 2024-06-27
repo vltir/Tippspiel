3f7a5249e9a27bf5e521a8f464569d7ee2efac9a
Etienne Bader <baderetienne@gmail.com>
2024-06-27 12:44:47 +0200

    fixed date and more

--- src/main/java/de/dhbwka/tippspiel/repositories/BenutzertippRepository.java
+++ src/main/java/de/dhbwka/tippspiel/repositories/BenutzertippRepository.java
@@ -3,7 +3,9 @@ package de.dhbwka.tippspiel.repositories;
 import de.dhbwka.tippspiel.entities.Benutzertipp;
 import de.dhbwka.tippspiel.entities.ERole;
 import de.dhbwka.tippspiel.entities.Role;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,4 +19,9 @@ public interface BenutzertippRepository extends JpaRepository<Benutzertipp, Long
 
     @Query("SELECT bt FROM Benutzertipp bt WHERE bt.username = :username AND bt.matchID = :matchID")
     Benutzertipp findByUsernameAndMatchID(@Param("username") String username, @Param("matchID") Long matchID);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Benutzertipp bt WHERE bt.username = :username AND bt.matchID = :matchID")
+    void deleteByUsernameAndMatchID(@Param("username") String username, @Param("matchID") Long matchID);
 }
