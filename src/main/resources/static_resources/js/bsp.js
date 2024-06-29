c721179814ac16cb1c4e405b92157629fd5e7fef
david <david.haslauer@yahoo.com>
2024-06-29 17:02:06 +0200

    adjusted the placement and made a the mobile view better

--- src/main/resources/static_resources/js/bsp.js
+++ src/main/resources/static_resources/js/bsp.js
@@ -3,7 +3,6 @@ let button = document.querySelector('#submit');
 let login = document.querySelector('#login');
 let sinup = document.querySelector('#signup');
 let form = document.querySelector('#form');
-
 // Event: Wenn jemand auf den Container "login | signup" klickt wechselt die Seite von login auf signup und zurück
 document.addEventListener("DOMContentLoaded", function() {
     var paragraph = document.getElementById("switch");
@@ -39,3 +38,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 */
+
